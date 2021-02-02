546ce22c81480e6051978b3f0afd611f0bad7e54
Mario <mariob360@gmail.com>
2021-02-01 19:16:11 -0800

    Added global Typography from styled components to Layout

--- pizza-gatsby/src/components/Layout.js
+++ pizza-gatsby/src/components/Layout.js
@@ -3,12 +3,14 @@ import Nav from './Nav';
 import Footer from './Footer';
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 const Layout = (props) => {
   const { children } = props;
   return (
     <div>
       <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
