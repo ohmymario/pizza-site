d0dde598bab58f20fac404cba701be08af08e66b
Mario <mariob360@gmail.com>
2021-02-01 18:05:06 -0800

    Added GlobalStyles from styled components to Layout

--- pizza-gatsby/src/components/Layout.js
+++ pizza-gatsby/src/components/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
 
 const Layout = (props) => {
   const { children } = props;
   return (
     <div>
+      <GlobalStyles />
       <Nav />
       {children}
       <Footer />
