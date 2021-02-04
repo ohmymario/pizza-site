3b5c6ca0a52596c7522573cfbe60190c1bd05d8e
Mario <mariob360@gmail.com>
2021-02-03 20:09:30 -0800

    Create the topping schema

---
+++ pizza-sanity/schemas/topping.js
@@ -0,0 +1,26 @@
+import { FaPepperHot } from 'react-icons/fa';
+
+export default {
+  // Computer Name
+  name: 'topping',
+  // Visible title
+  title: 'Toppings',
+  type: 'document',
+  icon: FaPepperHot,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'What is the name of the topping?',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+};
