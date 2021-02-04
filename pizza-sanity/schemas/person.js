e36f5a4a39964545b358c9ec59656d53b7e0a15a
Mario <mariob360@gmail.com>
2021-02-04 14:14:18 -0800

    create schema for employees/slicemasters

---
+++ pizza-sanity/schemas/person.js
@@ -0,0 +1,41 @@
+import { MdPerson } from 'react-icons/md';
+
+export default {
+  // Computer Name
+  name: 'person',
+  // Visible title
+  title: 'Slicemasters',
+  type: 'document',
+  icon: MdPerson,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      description: 'Name of the slicemaster',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'description',
+      type: 'text',
+      description: 'Tell us a bit about this person',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
