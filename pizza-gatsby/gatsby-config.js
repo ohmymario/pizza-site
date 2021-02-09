67c2edc8757071aed7d80492ec81d5a1c879bea2
Mario <mariob360@gmail.com>
2021-02-08 23:15:42 -0800

    Add styled components / sanity components connect sanity backend to gatsby

--- pizza-gatsby/gatsby-config.js
+++ pizza-gatsby/gatsby-config.js
@@ -1,7 +1,26 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
   siteMetadata: {
     title: `Gatsby`,
     siteUrl: `https://www.gatsbyjs.com`,
     description: `Blazing fast modern site generator for React`,
   },
+  plugins: [
+    // allows gatsby to use styled components in finding the critical CSS
+    'gatsby-plugin-styled-components',
+
+    // plugin to connect sanity and gatsby
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `u7kiam8u`,
+        dataset: `production`,
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
