413cd3c69b07ad03df106c2c7a1f93be671f55d2
Mario <mariob360@gmail.com>
2021-02-24 21:04:38 -0800

    create SEO component - metadata using react helmet

---
+++ pizza-gatsby/src/components/SEO.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const SEO = (props) => {
+  const { children, location, description, title, image } = props;
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `);
+  return (
+    <Helmet titleTemplate={`%s - ${site.siteMetadata.title}`}>
+      <html lang="en" />
+      <title>{title}</title>
+      {/* Fav icon */}
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="alternate icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width" initial-scale="1.0" />
+      <meta charset="utf-8" />
+      <meta name="description" content={site.siteMetadata.description} />
+
+      {/* Open Graph */}
+      {location && <meta property="og:url" content={location.href} />}
+      <meta property="og:image" content={image || '/logo.svg'} />
+      <meta property="og:title" content={title} key="ogtitle" />
+      <meta
+        propery="og:site_name"
+        content={site.siteMetadata.title}
+        key="ogsitename"
+      />
+      <meta property="og:description" content={description} key="ogdesc" />
+      {children}
+    </Helmet>
+  );
+};
+
+export default SEO;
