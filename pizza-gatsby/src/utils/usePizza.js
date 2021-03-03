b6e7d42e2d7bb9b8bf1c11d22f33c3ca77524f74
Mario <mariob360@gmail.com>
2021-03-02 19:07:11 -0800

    custom hook usePizza.js contains order, remove, add

---
+++ pizza-gatsby/src/utils/usePizza.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+const usePizza = (props) => {
+  const { pizzas, inputs } = props;
+
+  // 1. Create some state to hold our order
+  const [order, setOrder] = useState([]);
+
+  // 2. Make a function add things to order
+  function addToOrder(orderedPizza) {
+    setOrder([...order, orderedPizza]);
+  }
+
+  // 3. Make a function remove things from order
+  function removeFromOrder(index) {
+    setOrder([
+      // everything before the item we want to remove
+      ...order.slice(0, index),
+      // everything after the item we want to remove
+      ...order.slice(index + 1),
+    ]);
+  }
+
+  // TODO 4. Send this data to a serverless function when user checks out
+
+  return { order, addToOrder, removeFromOrder };
+};
+
+export default usePizza;
