53c8f3eeb08828683d1a28da5097a5ba7f7d1875
Mario <mariob360@gmail.com>
2021-03-02 18:13:04 -0800

    Fix MenuItemsStyles.js

--- pizza-gatsby/src/styles/MenuItemStyles.js
+++ pizza-gatsby/src/styles/MenuItemStyles.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 const MenuItemStyles = styled.div`
   display: grid;
   grid-template-columns: 100px 1fr;
-  grid-template-rows: 1fr 1fr;
+  /* grid-template-rows: 1fr 1fr; */
   gap: 0 1.3rem;
   align-content: center;
   align-items: center;
+  position: relative;
   .gatsby-image-wrapper {
     grid-row: span 2;
     height: 100%;
@@ -17,11 +18,9 @@ const MenuItemStyles = styled.div`
   button {
     font-size: 1.5rem;
   }
-
   button + button {
     margin-left: 1rem;
   }
-
   .remove {
     background: none;
     color: var(--red);
