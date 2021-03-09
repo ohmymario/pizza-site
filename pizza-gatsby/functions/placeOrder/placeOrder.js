9831fb8105b3070a8e7a43a1cade5b77e4c7d555
Mario <mariob360@gmail.com>
2021-03-08 18:44:49 -0800

    Check body has required fields / placeOrder.js

--- pizza-gatsby/functions/placeOrder/placeOrder.js
+++ pizza-gatsby/functions/placeOrder/placeOrder.js
@@ -10,13 +10,33 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-console.log('===============================');
-console.log(`Host ${process.env.MAIL_HOST}`);
-console.log(`USER ${process.env.MAIL_USER}`);
-console.log(`USER ${process.env.MAIL_PASS}`);
-console.log('===============================');
+// console.log('===============================');
+// console.log(`Host ${process.env.MAIL_HOST}`);
+// console.log(`USER ${process.env.MAIL_USER}`);
+// console.log(`USER ${process.env.MAIL_PASS}`);
+// console.log('===============================');
 
 exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body);
+  console.log(body);
+
+  // validate the data - check if its correct
+  const requiredFields = ['email', 'name', 'order'];
+  for (const field of requiredFields) {
+    console.log(`Checking that ${field} is good`);
+    if (!body[field]) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: `Oops you are missing the ${field} field`,
+        }),
+      };
+    }
+  }
+
+  // send the email
+  // send the success or error back to the
+
   // TESTING THE EMAILER
   const info = await transporter.sendMail({
     from: "Slick's Slices <slick@emample.com>",
