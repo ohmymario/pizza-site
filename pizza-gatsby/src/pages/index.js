ff4ed813e0ae8fffdfad8dc77d84abe83b8befe4
Mario <mariob360@gmail.com>
2021-03-11 20:07:07 -0800

    Create homepage using data from useLatestData()

--- pizza-gatsby/src/pages/index.js
+++ pizza-gatsby/src/pages/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
 
-const HomePage = () => (
-  <>
-    <p>I'm the homepage</p>
-  </>
-);
+function CurrentlySlicing() {
+  return <p>CurrentlySlicing</p>;
+}
+function HotSlices() {
+  return <p>HotSlices</p>;
+}
+
+const HomePage = (props) => {
+  const { slicemasters, hotSlices } = useLatestData();
+  return (
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
