1895380a04a276cc93be34e0a2ea18c70e173160
Mario <mariob360@gmail.com>
2021-03-15 19:49:36 -0700

    implement loading into both employee/food display

--- pizza-gatsby/src/components/CurrentlySlicing.js
+++ pizza-gatsby/src/components/CurrentlySlicing.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import LoadingGrid from './LoadingGrid';
 
-const CurrentlySlicing = (props) => <p>CurrentlySlicing</p>;
+const CurrentlySlicing = (props) => {
+  const { slicemasters } = props;
+  console.log(slicemasters);
+  return (
+    <div>
+      {/* LOADING STATE */}
+      {!slicemasters && <LoadingGrid count={4} />}
+      {/* LOADED BUT NO DATA */}
+      {slicemasters && !slicemasters?.length && (
+        <p>No one is working right now!</p>
+      )}
+      {/* LOADED AND WORKING */}
+    </div>
+  );
+};
 
 export default CurrentlySlicing;
--- pizza-gatsby/src/components/HotSlices.js
+++ pizza-gatsby/src/components/HotSlices.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import LoadingGrid from './LoadingGrid';
 
-const HotSlices = () => <p>HotSlices</p>;
+const HotSlices = (props) => {
+  const { hotSlices } = props;
+  console.log(props);
+  return (
+    <div>
+      {/* LOADING STATE */}
+      {!hotSlices && <LoadingGrid count={4} />}
+      {/* LOADED BUT NO DATA */}
+      {hotSlices && !hotSlices?.length && <p>Nothin' in the Case</p>}
+      {/* LOADED AND WORKING */}
+    </div>
+  );
+};
 
 export default HotSlices;
