40ded0317c6cb6e650bd94e4bb59513196ed9ae8
Mario <mariob360@gmail.com>
2021-03-17 14:46:30 -0700

    Fix navigation to work on smaller viewports

--- pizza-gatsby/src/components/Nav.js
+++ pizza-gatsby/src/components/Nav.js
@@ -40,9 +40,35 @@ const NavStyles = styled.nav`
   a {
     font-size: 3rem;
     text-decoration: none;
+    display: block;
     &:hover {
       color: var(--red);
     }
+    @media (max-width: 800px) {
+      font-size: 2rem;
+    }
+  }
+  @media (max-width: 600px) {
+    margin-bottom: 2rem;
+    padding-bottom: 2rem;
+    border-bottom: 2px solid var(--grey);
+
+    --columns: 4;
+    ul {
+      grid-template-rows: auto auto;
+      grid-template-columns: repeat(var(--columns), 1fr);
+      justify-items: center;
+    }
+    .logo-item {
+      order: 0;
+      grid-column: 1 / -1;
+    }
+    .logo {
+      transform: none;
+    }
+  }
+  @media (max-width: 500px) {
+    --columns: 2;
   }
 `;
 
@@ -55,7 +81,7 @@ const Nav = () => (
       <li>
         <Link to="/pizzas">Pizza Menus</Link>
       </li>
-      <li>
+      <li className="logo-item">
         <Link to="/">
           <Logo />
         </Link>
